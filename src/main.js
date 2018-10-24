e2decc17f246320e29d681de3360084fba9163b7
yaozehang <949806602@qq.com>
2018-10-24 22:00:47 +0800

    登录与首页

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'normalize.css/normalize.css' //引入初始化样式
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+import axios from './utils/axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios //全局导入axios
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
+const components = {
+  login: () => import ('@/views/Login'),
+  layout: () => import('@/views/Layout'),
+  home: () => import('@/views/Home')
+}
+
+const router =  new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      name:'login',
+      component:components.login,
+      meta:{
+        title:'登录'
+      }
+    },
+    {
+      path:'/layout',
+      component:components.layout,
+      redirect:'/layout/home',
+      children: [
+        {
+          path:'home',
+          name:'home',
+          component:components.home,
+          meta:{
+            title:'首页'
+          }
+        }
+      ]
     }
   ]
 })
+
+export default router
